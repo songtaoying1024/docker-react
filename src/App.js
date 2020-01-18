b235cb87e2b5db5a6fe1a9a76500ee7cbd6df830
Song taoying <lssty1992gmail.com>
2020-01-18 15:55:52 +0100

    changed app.js text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            stay foolish stay hungry
+            stay foolish stay hungry taoying( changes on feature branch)
         </p>
         <a
           className="App-link"
